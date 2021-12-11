2624690a9dcf8ced77713f137acede26d918e1b1
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-12-11 18:09:43 -0500

    altered subset of data, hope to fix non-linearity of model

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -5,12 +5,12 @@ pacman::p_load(readr, readxl, tidyverse, plyr, dplyr, ggplot2)
 Chess <- read_csv("Lichess_2013_2014_Complete.csv")
 #remove unwanted columns
 Chess <- Chess [-c(3,4,7,8,9,11,12,15)]
+#subset only 2014
+Chess <- filter(Chess, Year == 2014)
 #subset down to the fastest games
-Chess <- filter(Chess, TimeControl != "Classical")
-Chess <- filter(Chess, TimeControl != "Rapid")
+Chess <- filter(Chess, TimeControl == "Bullet")
 #filter for rating > 2000
-Chess <- filter(Chess, WhiteElo >= 2000)
-Chess <- filter(Chess, BlackElo >= 2000)
+
 #Simplify Opening Move
 Open <- str_split_fixed(Chess$Opening, ":", 2)
 Open1 <- Open [ ,(1)]
@@ -18,15 +18,17 @@ Chess$Opening <- Open1
 #Look at which openings are most frequently occuring in the data set
 open_freq <- tibble(count(Chess, 'Opening'))
 #Choose to do analysis on the top ten
-head(open_freq)
-top_ten <- open_freq %>% dplyr::filter(open_freq$freq > 9000)
+#top_ten <- open_freq %>% dplyr::filter(open_freq$freq > 9000) #original
+top_five <- open_freq %>% dplyr::filter(open_freq$freq > 460000)
 #top_ten <- tibble(count(Chess, 'Opening')) %>% filter(freq >= 9000)
 #Subset data set to only include top ten opening moves
-Chess <- semi_join(Chess, top_ten, by = "Opening")
+Chess <- semi_join(Chess, top_five, by = "Opening")
 
 #for visualization purposes
 Chess$Opening <- gsub(" ", "\n", Chess$Opening)
 Chess$Termination <- gsub(" ", "\n", Chess$Termination)
+
+#Chess$Winner <- ifelse(Chess$Winner == "White", 0, 1)
 
 # Questions: 
 # Does opening move have an affect on the total number of moves? 
